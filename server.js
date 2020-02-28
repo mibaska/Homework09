60d7b9d559bea06fa07f3f6f4586fcd91a4ce79b
Michael Baska <mikebaska1@gmail.com>
2020-02-28 13:51:52 -0600

    whythefuckthisnotworkingpleasegodhelp

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const Note = require("./note.js")
 
 var app = express();
@@ -7,8 +8,23 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 var notes = [];
+
+fs.readFile("db/db.json", "utf8", function(error, data) {
+
+  if (error) {
+    return console.log(error);
+  }
+
+  console.log(data);
+
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.get("/api/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "notes.html"));
